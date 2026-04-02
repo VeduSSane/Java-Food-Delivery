99a43c1a46cbc011cfbd362f55c165cffb944a6c
Vedant Sandeep Sane <vedantssiot125@gst.sies.edu.in>
2026-04-02 19:26:18 +0530

    added FoodItem class
    made the use of constructors and used setters for setting price and name ,with the help of oopm.

---
+++ FoodItem.java
@@ -0,0 +1,18 @@
+class FoodItem {
+  String name ;
+  float price ;
+
+    FoodItem( String name , float price ) {
+      this.name = name;
+      this.price = price;
+    }//end of constructor
+
+      setname() {
+        return name;
+      }//end of setter name
+  
+      setprice() {
+        return price;
+      }//end of setter price
+  
+}//end of class
