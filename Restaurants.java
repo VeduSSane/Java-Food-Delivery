bebe5611525a8f462a7657bac4ee4b8874726457
Vedant Sandeep Sane <vedantssiot125@gst.sies.edu.in>
2026-04-02 20:01:55 +0530

    added half incomplete restaurant class

---
+++ Restaurants.java
@@ -0,0 +1,18 @@
+class Restaurant {
+
+  String name ;
+
+  Restaurant(String name) {
+
+    this.name= name;
+    
+  }//end of constructor restaurant
+
+  setname() { 
+  return name;
+  }//end of restaurnat name settee
+
+     FoodItem[] menu = new FoodItem[10];
+    int count = 0;
+
+}//end of class restaurnat
