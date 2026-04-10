a10424ced57b2b0026604090f3fbc527240ac9d6
Manthan01 <manthanpatil1127@gmail.com>
2026-04-11 01:08:52 +0530

    Create FoodItem.java
    This class stores food item name and price using constructor overloading.

---
+++ FoodItem.java
@@ -0,0 +1,17 @@
+class FoodItem 
+{
+    String name;
+    double price;
+    //constructor overloading
+    FoodItem(String name, float price) 
+	{
+        this.name = name;
+        this.price = price;
+    }
+
+    FoodItem(String name, double price) 
+	{
+        this.name = name;
+        this.price = price;
+    }
+}//End of class FoodItem
